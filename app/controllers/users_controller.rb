93c9d46129ca114f43b9063c3cd454ab6131e821
Peter Alderson <peter.alderson@gmail.com>
2014-08-17 16:49:52 -0700

    prof public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,19 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
   end
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
     end
   end
-
 end
